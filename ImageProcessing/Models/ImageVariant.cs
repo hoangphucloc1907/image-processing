bd1f9c80169de4eb2a480a5b84b3f0564be275a5
hoangphucloc <hoangloc0706@gmail.com>
2025-03-08 14:35:17 +0700

    update

---
+++ ImageProcessing/Models/ImageVariant.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ImageProcessing.Models
+{
+    public class ImageVariant
+    {
+        public int Id { get; set; }
+        public string OriginalFileName { get; set; }
+        public string FilePath { get; set; }
+        public VariantType Type { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Format { get; set; }
+        public long FileSize { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Quality { get; set; }
+    }
+
+    public enum VariantType
+    {
+        Original,
+        TwoK,
+        WebOptimized,
+        MobileOptimized,
+        Thumbnail
+    }
+}
---
+++ ImageProcessing/Services/IImageProcessingService.cs
@@ -0,0 +1,6 @@
+﻿namespace ImageProcessing.Services
+{
+    public interface IImageProcessingService
+    {
+    }
+}
