9b5fd2df367aa7955078834fa6023691a19b60ae
hoangphucloc <hoangloc0706@gmail.com>
2025-03-08 14:41:36 +0700

    update db

---
+++ ImageProcessing/ImageDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ImageProcessing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageProcessing
+{
+    public class ImageDbContext : DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ImageVariant> ImageVariants { get; set; }
+    }
+}
